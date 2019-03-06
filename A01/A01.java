df2a3212663e4c4282616cce8428e491bfea123e
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-03-05 23:51:01 -0700

    A01 assignment

---
+++ A01/A01.java
@@ -0,0 +1,36 @@
+/*****************************************
+* Author : Ahmad Rasoulpour
+* Date : 09/07/2017
+* Assignment: A01-Print
+******************************************/
+
+public class A01
+{
+   public static void main(String [] args)
+   {
+      System.out.println("What I hope to get out of this class:");
+      System.out.println("I always had a dream been a programmer. I have 10 years experience, "+"\n"+
+      "but most of them are about networking. I had couple courses about programming"+"\n"+
+      "but I didn't get enough and deep information about fundamentals. but now I "+"\n"+
+      "have another chance, and this time I don't wanna miss even one word about "+"\n"+
+      "programming and becuase of this I took this class becuase I wanna use every "+"\n"+
+      "second of it and learn as much as I can, and prove to myself that " +"\n"+ 
+      "I caaaaaaaaaan be a progorammer." );
+      
+      System.out.println();
+      
+      System.out.println("What I am willing to put into this class:");
+      System.out.println("when we have goal and we wanna catch it, we should do anything "+"\n"+
+      "for that. nothing shouldn't be reason to give up. becuase of this, I'm "+"\n"+
+      "gonna do anything for programming. read the book, doing all the homwork "+"\n"+
+      "and the assignments, listen to word by word on the class to learn more."+"\n"+
+      "trying to put my whole mind on the programming and on this class. and I will do"+"\n"+
+      "any help as much as I can to my classmates");
+      
+      for (int i = 1; i <= 19; i+=6)
+      {
+          System.out.printf("%d ", i);
+      }
+      
+   } 
+}
